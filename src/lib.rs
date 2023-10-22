02418d60c90892da8c0d5d900b144f7abb71ea4b
Alexandru Moise <alexandru.t.moise@gmail.com>
2023-10-22 18:29:24 +0200

    messing around some more with html, also fix formating

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 use winit::{
+    dpi::PhysicalSize,
     event::*,
     event_loop::{ControlFlow, EventLoop},
-    window::{WindowBuilder, Window}, dpi::PhysicalSize,
+    window::{Window, WindowBuilder},
 };
 
-#[cfg(target_arch="wasm32")]
-use wasm_bindgen::prelude::*;
-#[cfg(target_arch="wasm32")]
+#[cfg(target_arch = "wasm32")]
 use log::info;
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
 
 struct State {
     surface: wgpu::Surface,
@@ -38,7 +39,7 @@ impl State {
         // # Safety
         // The surface needs to live as long as the window that created it
         // State owns the window so this should be safe
-        let surface = unsafe {instance.create_surface(&window)}.unwrap();
+        let surface = unsafe { instance.create_surface(&window) }.unwrap();
 
         /*
         // NOTE: NAVAIL on WASM
@@ -47,102 +48,111 @@ impl State {
             adapter.is_surface_supported(&surface)
         }).unwrap(); */
 
-        let adapter = instance.request_adapter(
-            &wgpu::RequestAdapterOptions {
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
                 power_preference: wgpu::PowerPreference::default(),
                 compatible_surface: Some(&surface),
                 force_fallback_adapter: false,
-            },
-        ).await.unwrap();
+            })
+            .await
+            .unwrap();
 
-        let (device, queue) = adapter.request_device(
-            &wgpu::DeviceDescriptor {
-                features: wgpu::Features::empty(),
-                limits: if cfg!(target_arch = "wasm32") {
-                    wgpu::Limits::downlevel_webgl2_defaults()
-                } else {
-                    wgpu::Limits::default()
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: wgpu::Features::empty(),
+                    limits: if cfg!(target_arch = "wasm32") {
+                        wgpu::Limits::downlevel_webgl2_defaults()
+                    } else {
+                        wgpu::Limits::default()
+                    },
+                    label: None,
                 },
-                label: None,
-            },
-            None).await.unwrap();
+                None,
+            )
+            .await
+            .unwrap();
 
-            let surface_caps = surface.get_capabilities(&adapter);
+        let surface_caps = surface.get_capabilities(&adapter);
 
-            let surface_format = surface_caps.formats.iter()
-                .copied()
-                .find(|f| f.is_srgb())
-                .unwrap_or(surface_caps.formats[0]);
+        let surface_format = surface_caps
+            .formats
+            .iter()
+            .copied()
+            .find(|f| f.is_srgb())
+            .unwrap_or(surface_caps.formats[0]);
 
-            let config = wgpu::SurfaceConfiguration {
-                usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-                format: surface_format,
-                width: size.width,
-                height: size.height,
-                present_mode: surface_caps.present_modes[0],
-                alpha_mode: surface_caps.alpha_modes[0],
-                view_formats: vec![],
-            };
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+        };
 
-            surface.configure(&device, &config);
+        surface.configure(&device, &config);
 
-            let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-                label: Some("Shader"),
-                source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
-            });
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("Shader"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        });
 
-            let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: Some("Render Pipeline Layout"),
                 bind_group_layouts: &[],
                 push_constant_ranges: &[],
             });
 
-            let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor{
-                label: Some("Render Pipeline"),
-                layout: Some(&render_pipeline_layout),
-                vertex: wgpu::VertexState {
-                    module: &shader,
-                    entry_point: "vs_main", // 1
-                    buffers: &[], // 2
-                },
-                fragment: Some(wgpu::FragmentState { // 3
-                    module: &shader,
-                    entry_point: "fs_main",
-                    targets: &[Some(wgpu::ColorTargetState {
-                        format: config.format,
-                        blend: Some(wgpu::BlendState::REPLACE),
-                        write_mask: wgpu::ColorWrites::ALL,
-                    })]
-                }),
-                primitive: wgpu::PrimitiveState {
-                    topology: wgpu::PrimitiveTopology::TriangleList,
-                    strip_index_format: None,
-                    front_face: wgpu::FrontFace::Ccw,
-                    cull_mode: Some(wgpu::Face::Back),
-                    polygon_mode: wgpu::PolygonMode::Fill,
-                    unclipped_depth: false,
-                    conservative: false,
-                },
-                depth_stencil: None,
-                multisample: wgpu::MultisampleState {
-                    count: 1,
-                    mask: !0,
-                    alpha_to_coverage_enabled: false,
-                },
-                multiview: None,
-            });
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main", // 1
+                buffers: &[],           // 2
+            },
+            fragment: Some(wgpu::FragmentState {
+                // 3
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+        });
 
-            Self {
-                window,
-                surface,
-                device,
-                queue,
-                config,
-                size,
-                cur_x: 0.0,
-                cur_y: 0.0,
-                render_pipeline
-            }
+        Self {
+            window,
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            cur_x: 0.0,
+            cur_y: 0.0,
+            render_pipeline,
+        }
     }
 
     pub fn window(&self) -> &Window {
@@ -162,26 +172,29 @@ impl State {
         false
     }
 
-    fn update(&mut self) {
-    }
+    fn update(&mut self) {}
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
         let output = self.surface.get_current_texture()?;
 
-        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
 
-        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-            label: Some("Render Encoder")
-        });
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
 
         {
             let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("Render Pass"),
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &view, 
-                    resolve_target: None, 
+                    view: &view,
+                    resolve_target: None,
                     ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color{
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
                             r: 0.1 * (self.window().inner_size().width as f64 / self.cur_x),
                             g: 0.2 * (self.window().inner_size().width as f64 / self.cur_x),
                             b: 0.3 * (self.window().inner_size().width as f64 / self.cur_x),
@@ -204,7 +217,7 @@ impl State {
     }
 }
 
-#[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub async fn run() {
     cfg_if::cfg_if! {
         if #[cfg(target_arch="wasm32")] {
@@ -220,17 +233,18 @@ pub async fn run() {
     #[cfg(target_arch = "wasm32")]
     {
         use winit::dpi::PhysicalSize;
-        window.set_inner_size(PhysicalSize::new(450,400));
+        window.set_inner_size(PhysicalSize::new(450, 400));
 
         use winit::platform::web::WindowExtWebSys;
         web_sys::window()
             .and_then(|win| win.document())
             .and_then(|doc| {
-                let dst = doc.get_element_by_id("wasm-example")?;
+                let dst = doc.get_element_by_id("rust-wasm-wgpu")?;
                 let canvas = web_sys::Element::from(window.canvas());
                 dst.append_child(&canvas).ok()?;
                 Some(())
-            }).expect("Couldn't append canvas to document body");
+            })
+            .expect("Couldn't append canvas to document body");
     }
 
     #[cfg(target_arch = "wasm32")]
@@ -240,61 +254,71 @@ pub async fn run() {
     event_loop.run(
         //Start of closure
         move |event, _, control_flow| {
-        // Closure has one match statement
-        match event {
-            Event::RedrawRequested(window_id) if window_id == state.window().id() => {
-                state.update();
-                match state.render() {
-                    Ok(_) => {}
-                    // Reconfigure the surface if lost
-                    Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
-                    // The system is out of memory, we should quit
-                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-                    // All other errors (Outdated, Timeout) should be resolved by the next frame
-                    Err(e) => eprintln!("{:?}", e),
+            // Closure has one match statement
+            match event {
+                Event::RedrawRequested(window_id) if window_id == state.window().id() => {
+                    state.update();
+                    match state.render() {
+                        Ok(_) => {}
+                        // Reconfigure the surface if lost
+                        Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                        // The system is out of memory, we should quit
+                        Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                        // All other errors (Outdated, Timeout) should be resolved by the next frame
+                        Err(e) => eprintln!("{:?}", e),
+                    }
                 }
-            }
-            Event::MainEventsCleared => {
-                // RedrawRequested will only trigger once, unless we manually request it
-                state.window().request_redraw();
-            }
-            Event::WindowEvent {
-                ref event,
-                window_id
-            } if window_id == state.window().id() => if !state.input(event) {
-                // Inner match statement
-                match event {
-                    // Sometimes I wish match expressions were fall-through
-                    WindowEvent::CloseRequested | WindowEvent::KeyboardInput {
-                                    input: KeyboardInput { // This KeyboardInput struct is different from the one above
+                Event::MainEventsCleared => {
+                    // RedrawRequested will only trigger once, unless we manually request it
+                    state.window().request_redraw();
+                }
+                Event::WindowEvent {
+                    ref event,
+                    window_id,
+                } if window_id == state.window().id() => {
+                    if !state.input(event) {
+                        // Inner match statement
+                        match event {
+                            // Sometimes I wish match expressions were fall-through
+                            WindowEvent::CloseRequested
+                            | WindowEvent::KeyboardInput {
+                                input:
+                                    KeyboardInput {
+                                        // This KeyboardInput struct is different from the one above
                                         state: ElementState::Pressed,
                                         virtual_keycode: Some(VirtualKeyCode::Escape),
                                         ..
                                     },
-                        ..
-                    } => {
-                        *control_flow = ControlFlow::Exit;
-                        #[cfg(target_arch = "wasm32")]
-                        info!("Pressed ESC");
-                    },
-                    WindowEvent::Resized(physical_size) => {
-                        println!("physical_size: {:?}", physical_size);
-                        #[cfg(target_arch = "wasm32")]
-                        info!("physical_size: {:?}", physical_size);
-                        state.resize(*physical_size);
-                    },
-                    WindowEvent::ScaleFactorChanged { new_inner_size, ..} => {
-                        state.resize(**new_inner_size);
-                    },
-                    WindowEvent::CursorMoved { device_id, position, modifiers } => {
-                        state.cur_x = position.x;
-                        state.cur_y = position.y;
+                                ..
+                            } => {
+                                *control_flow = ControlFlow::Exit;
+                                #[cfg(target_arch = "wasm32")]
+                                info!("Pressed ESC");
+                            }
+                            WindowEvent::Resized(physical_size) => {
+                                println!("physical_size: {:?}", physical_size);
+                                #[cfg(target_arch = "wasm32")]
+                                info!("physical_size: {:?}", physical_size);
+                                state.resize(*physical_size);
+                            }
+                            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                                state.resize(**new_inner_size);
+                            }
+                            WindowEvent::CursorMoved {
+                                device_id,
+                                position,
+                                modifiers,
+                            } => {
+                                state.cur_x = position.x;
+                                state.cur_y = position.y;
+                            }
+                            _ => {}
+                        }
                     }
-                    _ => {}
                 }
-            },
-            // Second outer match case
-            _ => {},
-        }
-    });
+                // Second outer match case
+                _ => {}
+            }
+        },
+    );
 }
